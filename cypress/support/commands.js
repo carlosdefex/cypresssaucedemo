1223e7130212a822ae3daa7493e821685181bbcd
Carlos De Fex <carlos.defex@Carlos-Jose-Defexs-Mac.local>
2024-06-21 12:12:13 -0500

    resolve conflicts

---
+++ cypress/support/commands.js
@@ -0,0 +1,8 @@
+// Import task command modules
+import {saucedemoLogin} from '../tasks/LoginTask';
+
+// Import question command modules
+import {loginSucess} from '../questions/ValidationHome';
+
+export { loginSucess }
+export { saucedemoLogin }
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,8 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import '../tasks/LoginTask'
-import '../questions/ValidationHome'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
